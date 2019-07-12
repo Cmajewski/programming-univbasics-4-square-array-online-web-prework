287712a363a38c50e555a545f0dd2d5cb7f53466
peaceful-cyborg-2337 <cmajewski@wesleyan.edu>
2019-07-12 23:51:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,8 @@
 def square_array(array)
-  counter=0
-  new_array=[]
-  while counter<array.length do
-  array[counter]**2
-    counter+=1
-
-  end
-  
-  return  new_array
+counter=0
+while array[counter]do
+  return array[counter]*2
+  counter+=1
+end
 
 end
